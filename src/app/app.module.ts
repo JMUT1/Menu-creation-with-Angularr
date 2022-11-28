a8c83517d0407f6c8b5f0f76197bf2c70a9ddb34
Chema Torres <chemaaa94@gmail.com>
2022-11-28 12:32:22 -0600

    creating components for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
+import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoopingListComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
