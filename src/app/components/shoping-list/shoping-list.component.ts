39f49928e79e753833bc862f74562e6c1cd4ccea
Chema Torres <chemaaa94@gmail.com>
2022-11-28 16:21:38 -0600

    finishin menu page...without functionality

--- src/app/components/shoping-list/shoping-list.component.ts
+++ src/app/components/shoping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredients} from '../../shared/ingredient.model'
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
+
+  ingredients: Ingredients[] = [new Ingredients('Apples',5), new Ingredients('Tomatos', 10)];
 
   constructor() { }
 
