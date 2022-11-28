158df2360bcacf3f6e51e51c1d87b8b8914d48cd
Chema Torres <chemaaa94@gmail.com>
2022-11-28 12:39:32 -0600

    creating new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
 import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoopingListComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
