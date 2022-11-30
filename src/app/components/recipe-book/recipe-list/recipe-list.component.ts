d02fd9504ed818e7eb41126b4df133b659a56fbc
Chema Torres <chemaaa94@gmail.com>
2022-11-30 09:56:58 -0600

    item ahora puede hacer click y mostrar el detalle del producto

--- src/app/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +6,7 @@ import {Recipe} from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
 
 recipes: Recipe[] = [new Recipe('A test Recipe', 'Simple a test', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'), new Recipe('A test Recipe', 'Simple a test', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')];
 
@@ -14,7 +15,10 @@ recipes: Recipe[] = [new Recipe('A test Recipe', 'Simple a test', 'https://asset
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRecipeSelected(item: Recipe){
+    this.recipeWasSelected.emit(item)
   }
 
 }
