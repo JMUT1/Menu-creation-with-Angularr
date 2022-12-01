c57855cbf379051ec5c6212c25baf4dce4fa5ab1
Chema Torres <chemaaa94@gmail.com>
2022-12-01 12:09:11 -0600

    drowpdown file

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -11,6 +11,6 @@ export class DropDownDirective{
   // }
 
   @HostListener('document:click', ["$event"]) toggleOpen(event: Event)
-    { this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen: false}
+    { this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false}
   constructor(private elRef: ElementRef){}
 }
